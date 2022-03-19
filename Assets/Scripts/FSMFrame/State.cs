571718cb4b8a29d7e702da1f5edf9216418c956c
Axiejinhao <xjh9836@gamil.com>
2022-03-19 09:14:11 +0800

    FSMFrame

---
+++ Assets/Scripts/FSMFrame/State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace FSM
+{
+    public class State : MonoBehaviour
+    {
+
+    }
+}
